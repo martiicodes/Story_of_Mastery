1605799b48a45015edaf94886256d70421d307d3
Nenad Milcanovic <nenadmilcanovic@proton.me>
2024-11-16 18:11:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+// Read the Blog button
 function goToBlog() {
     window.location.href = 'posts.html';
   }
 
+// Google Analytics Code
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DESP7H7330');
+</script>
   
